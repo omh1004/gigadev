d773a5671bbc6dba1d761721f3354abd41d1fbcc
ahnyoonsang <ahnyoonsang47@gmail.com>
2025-03-11 14:31:59 +0900

    ys_인트로2레이아웃수정

--- src/components/mainmenu/intro/introstart/IntroStart2.vue
+++ src/components/mainmenu/intro/introstart/IntroStart2.vue
@@ -102,7 +102,7 @@ export default {
   margin-bottom: 20px;
   min-height: 150px;
   display: flex;
-
+  border-radius: 24px;
   align-items: center;
   justify-content: center;
   box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
