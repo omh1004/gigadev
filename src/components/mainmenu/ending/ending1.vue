e653042e10b5d82bcf74092320a1eac30ff1f945
ahnyoonsang <ahnyoonsang47@gmail.com>
2025-03-17 17:03:14 +0900

    ys_엔딩메세지작업

--- src/components/mainmenu/ending/ending1.vue
+++ src/components/mainmenu/ending/ending1.vue
@@ -1,33 +1,36 @@
 <template>
   <div class="maincontainer">
     <div class="dialog-box">
-      <div class="dialog-text">{{ inputText }}</div>
+      <div class="dialog-text" v-html="inputText"></div>
       <div class="dialog-arrow" @click="movePage">▼</div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
-  name:'ending1',
-  data(){
+  name: 'ending1',
+  data() {
     return {
-      inputText:''
-    }
-  },
-  methods: {
-
+      inputText: ''
+    };
   },
   mounted() {
-    let innerText = '난..모든 걸 다 잃었어.. 거리로 나온지 벌써 10년이 지났지 한때는 나도 잘 될 줄 알았어 아니, 잘 될 수밖에 없다고 믿었지';
-      
-        for(let i =0;i<innerText.length;i++){
-          setTimeout(()=>{
-            this.inputText+=innerText[i];
-          },i*50)
+    let innerText = '난.. 모든 걸 다 잃었어.. 거리로 나온지 벌써 10년이 지났지\n 한때는 나도 잘 될 줄 알았어 아니, 잘 될 수밖에 없다고 믿었지';
+
+    for (let i = 0; i < innerText.length; i++) {
+      setTimeout(() => {
+        if (innerText[i] === '\n') {
+          this.inputText += '<br>';
+        } else {
+          this.inputText += innerText[i];
         }
+      }, i * 50);
+    }
   }
-}
+};
 </script>
+
 <style scoped>
   .main-container {
     width: 100%;
--- src/components/mainmenu/ending/ending2.vue
+++ src/components/mainmenu/ending/ending2.vue
@@ -1,32 +1,34 @@
 <template>
   <div class="maincontainer">
     <div class="dialog-box">
-      <div class="dialog-text">{{ inputText }}</div>
+      <div class="dialog-text" v-html="inputText"></div>
       <div class="dialog-arrow" @click="movePage">▼</div>
     </div>
   </div>
 </template>
+
 <script>
 export default {
-  name:'ending2',
-  data(){
+  name: 'ending2',
+  data() {
     return {
-      inputText:''
-    }
-  },
-  methods: {
-
+      inputText: ''
+    };
   },
   mounted() {
-    let innerText = '그럭저럭.. 잘 살아 남은 것 같아 대출은 겨우 다 갚았고 매출은 딱 밥 벌어 먹을 만큼 나와';
-      
-        for(let i =0;i<innerText.length;i++){
-          setTimeout(()=>{
-            this.inputText+=innerText[i];
-          },i*50)
+    let innerText = '그럭저럭.. 잘 살아 남은 것 같아\n 대출은 겨우 다 갚았고 매출은 딱 밥 벌어 먹을 만큼 나와';
+
+    for (let i = 0; i < innerText.length; i++) {
+      setTimeout(() => {
+        if (innerText[i] === '\n') {
+          this.inputText += '<br>';
+        } else {
+          this.inputText += innerText[i];
         }
+      }, i * 50);
+    }
   }
-}
+};
 </script>
 <style scoped>
   
