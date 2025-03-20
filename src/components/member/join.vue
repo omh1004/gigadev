e4aba438f1644c6083ffdc1ec682a62b44d3f0c7
udaoewka <ksm54239@gmail.com>
2025-03-20 10:15:49 +0900

    회원가입과 로그인 localhost를 서버주소로 바꿈꿈

--- src/components/member/join.vue
+++ src/components/member/join.vue
@@ -121,7 +121,7 @@ export default {
     checkIdDuplicate() {
   if (!this.isIdValid) return;
   
-  fetch('http://localhost:8080/spring/api/checkId', {
+  fetch('http://3.38.185.252:8080/spring/api/checkId', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
@@ -151,7 +151,7 @@ export default {
         return;
       }
       
-      fetch('http://localhost:8080/spring/api/enrollMember', {
+      fetch('http://3.38.185.252:8080/spring/api/enrollMember', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
